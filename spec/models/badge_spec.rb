e36b2f4d974c68365526cd32b7f56e9ab9d99abe
Pat Holland <pholls@users.noreply.github.com>
2017-02-18 16:13:52 -0800

    migrate to change column name, pass badge specs

--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Badge, type: :model do
   it { is_expected.to have_many :users }
   it { is_expected.to have_many :badges_users }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_presence_of :image_id }
+  it { is_expected.to validate_numericality_of :image_id }
 end
