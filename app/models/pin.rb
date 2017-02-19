8aa1c9edd04c1468e3cd18f1bc016fdaf7c3c17e
Pat Holland <pholls@users.noreply.github.com>
2017-02-19 12:21:21 -0800

    pin creation cleanup

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :user_id, :category, :description, :lat, :lng
+
+  # validates_inclusion_of :category, in: ['', '', ]
 end
